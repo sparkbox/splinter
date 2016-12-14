8c48c773f9a866f4f945a861bb88c96cab93268c
Adam Simpson <adam@heysparkbox.com>
2016-12-14 08:32:21 -0500

    refactor: pull walk functions into their own funcs

--- parseSCSS.js
+++ parseSCSS.js
@@ -8,40 +8,50 @@ const parser = (css) => {
   const splits = [];
 
   /**
+  * @param {object} at - an at rule, e.g. @media
+  */
+  const parseAtRule = (at) => {
+    if (/brand/.test(at.params)) {
+      if (at.parent.type === 'atrule') {
+        splits.push(`@${at.parent.name} ${at.parent.params} {`);
+        splits.push(at.toString());
+        splits.push('}');
+      } else {
+        splits.push(`${at.parent.selector} {`);
+        splits.push(at.toString());
+        splits.push('}');
+      }
+
+      at.remove();
+    }
+  };
+
+  /**
+  * @param {object} rule - a CSS rule: a selector followed by a declaration block
+  */
+  const parseDecl = (rule) => {
+    rule.walkDecls(decl => {
+      if (/brand-/.test(decl.value)) {
+        if (rule.parent.type === 'atrule') {
+          splits.push(`@${rule.parent.name} ${rule.parent.params} {`);
+          splits.push(`${rule.selector} { ${decl.toString()}; }`);
+          splits.push('}');
+        } else {
+          splits.push(`${rule.selector} { ${decl.toString()}; }`);
+        }
+
+        decl.remove();
+      }
+    });
+  };
+
+  /**
   * @returns {Function}  - PostCSS function
   */
   const brandParse = postcss.plugin('brand-parse', (options) => {
     return (global, result) => {
-      global.walkAtRules(at => {
-        if (/brand/.test(at.params)) {
-          if (at.parent.type === 'atrule') {
-            splits.push(`@${at.parent.name} ${at.parent.params} {`);
-            splits.push(at.toString());
-            splits.push('}');
-          } else {
-            splits.push(`${at.parent.selector} {`);
-            splits.push(at.toString());
-            splits.push('}');
-          }
-
-          at.remove();
-        }
-      });
-
-      global.walkRules(rule => {
-        rule.walkDecls(decl => {
-          if (/brand-/.test(decl.value)) {
-            if (rule.parent.type === 'atrule') {
-              splits.push(`@${rule.parent.name} ${rule.parent.params} {`);
-              splits.push(`${rule.selector} { ${decl.toString()}; }`);
-              splits.push('}');
-            } else {
-              splits.push(`${rule.selector} { ${decl.toString()}; }`);
-            }
-            decl.remove();
-          }
-        });
-      });
+      global.walkAtRules(x => parseAtRule(x));
+      global.walkRules(x => parseDecl(x));
     };
   });
 
@@ -54,10 +64,8 @@ const parser = (css) => {
     //use the bubble option to specify mixins to unwrap
     const processor = postcss([nest({bubble: ['brand']}), brandParse()]);
 
-    return processor.process(css, {syntax})
-    .then(x => {
-      return {css: x.css, splits: splits};
-    });
+    return processor.process(css, { syntax })
+    .then(x => ({ css: x.css, splits: splits }));
   };
 
   return parseSCSS(css);
